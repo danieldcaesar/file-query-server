c3ca288f8dd24f7a9a050afbf7d83a0210c71b66
danieldcaesar <danieldcaesar@gmail.com>
2021-09-30 12:03:07 -0400

    put function working
    Server expects put command on startup, but it successfully transfers the file.

--- client/client.py
+++ client/client.py
@@ -4,12 +4,29 @@ PORT = 9999
 SERVER = 'localhost'
 ADDR = (SERVER, PORT)
 FORMAT = 'UTF-8'
+BUFFER = 4096
+
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
-
-
-msg = client.recv(2048)
+connected = True
+msg = client.recv(BUFFER)
 print(msg.decode())
+
+data = input("Enter the name of a text file eg. PUT sample.txt: ")
+filename = data[4:]
+client.send(bytes(filename, FORMAT))
+file = open(filename, 'rb')
+data = file.read(BUFFER)
+
+while data:
+    client.send(data)
+    data = file.read(BUFFER)
+
+print(f'[SENT] File \'{filename}\' sent.')
+
+
+
 
 client.close()
--- server/server.py
+++ server/server.py
@@ -1,24 +1,34 @@
 import socket
+import shutil
 
 PORT = 9999
 SERVER = 'localhost'
 ADDR = (SERVER, PORT)
 FORMAT = 'UTF-8'
-
-host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host.bind(ADDR)
+BUFFER = 4096
 
 
 def start():
     host.listen(1)
+    
     while True:
         conn, addr = host.accept()
         print(f"[NEW CONNECTION] client {addr} is connected.")
+        conn.send(bytes(f'[CONNECTED] Connected to server {SERVER}:{PORT}', FORMAT))
 
-        conn.send(bytes(f'[CONNECTED] Connected to server with {addr}', FORMAT))
+        filename = conn.recv(BUFFER).decode(FORMAT)
+        file = open(f'server/{filename}', 'wb')
+        data = conn.recv(BUFFER)
+        while data:
+            file.write(data)
+            data = conn.recv(BUFFER)
+        file.close()
+        print(f'[RECEIVED] Received file \'{filename}\'')
         conn.close()
-    return
+        
+        break        
 
-
-print("[STARTING] Server is starting...")
+host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host.bind(ADDR)
+print("[START] Server has started.")
 start()
