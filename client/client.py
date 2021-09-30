5fc9d8173124e2dcd545329d2e5987092125b74c
danieldcaesar <danieldcaesar@gmail.com>
2021-09-30 17:00:13 -0400

    exit & put functions
    Both are fully functional however, PUT does not catch errors in formatting

--- client/client.py
+++ client/client.py
@@ -5,8 +5,22 @@ SERVER = 'localhost'
 ADDR = (SERVER, PORT)
 FORMAT = 'UTF-8'
 BUFFER = 4096
+SEPARATOR = '<SEPARATOR>'
 
+def exit():
+    client.send(bytes('EXIT', FORMAT))
 
+def put(cmd):
+    filename = cmd[4:]
+    client.send(bytes(filename, FORMAT))
+    file = open(filename, 'rb')
+    data = file.read(BUFFER)
+
+    while data:
+        client.send(data)
+        data = file.read(BUFFER)
+
+    print(f'[SENT] File \'{filename}\' sent.')
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
@@ -14,19 +28,15 @@ connected = True
 msg = client.recv(BUFFER)
 print(msg.decode())
 
-data = input("Enter the name of a text file eg. PUT sample.txt: ")
-filename = data[4:]
-client.send(bytes(filename, FORMAT))
-file = open(filename, 'rb')
-data = file.read(BUFFER)
 
-while data:
-    client.send(data)
-    data = file.read(BUFFER)
-
-print(f'[SENT] File \'{filename}\' sent.')
-
-
-
+while connected:
+    command = input("Enter command to be executed: ")
+    if command == 'EXIT':
+        exit()
+        connected = False
+    if command[:3] == 'PUT':
+        client.send(bytes('PUT', FORMAT))
+        put(command)
 
 client.close()
+
--- server/server.py
+++ server/server.py
@@ -1,34 +1,40 @@
 import socket
-import shutil
 
 PORT = 9999
 SERVER = 'localhost'
 ADDR = (SERVER, PORT)
 FORMAT = 'UTF-8'
 BUFFER = 4096
+SEPARATOR = '<SEPARATOR>'
 
 
-def start():
-    host.listen(1)
-    
-    while True:
-        conn, addr = host.accept()
-        print(f"[NEW CONNECTION] client {addr} is connected.")
-        conn.send(bytes(f'[CONNECTED] Connected to server {SERVER}:{PORT}', FORMAT))
 
-        filename = conn.recv(BUFFER).decode(FORMAT)
-        file = open(f'server/{filename}', 'wb')
-        data = conn.recv(BUFFER)
-        while data:
-            file.write(data)
-            data = conn.recv(BUFFER)
-        file.close()
-        print(f'[RECEIVED] Received file \'{filename}\'')
-        conn.close()
-        
-        break        
+def put():
+    filename = conn.recv(BUFFER).decode(FORMAT)
+    file = open(f'server/{filename}', 'wb')
+    data = conn.recv(BUFFER)
+    file.write(data)
+    file.close()
+    print(f'[RECEIVED] Received file \'{filename}\'')
 
 host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host.bind(ADDR)
 print("[START] Server has started.")
-start()
+host.listen(1)
+connected = True
+while connected:
+        conn, addr = host.accept()
+        print(f"[NEW CONNECTION] client {addr} is connected.")
+        conn.send(bytes(f'[CONNECTED] Connected to server {SERVER}:{PORT}', FORMAT))
+
+        wait_command = True
+        while wait_command:
+            command = conn.recv(BUFFER).decode(FORMAT)
+            if command == 'EXIT':
+                print('[EXIT] Client has ended connection.')
+                connected = False
+                break
+            if command == 'PUT':
+                put()
+
+conn.close()
